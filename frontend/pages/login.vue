f84c155e82ee2ebcebfd433450bcebe674fdcad6
ChristianGreiner <christian.greiner@posteo.net>
2022-04-12 18:39:45 +0200

    Implement sign up view

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -4,19 +4,19 @@
     <div class="text-center mb-4">
       <a href="." class="navbar-brand navbar-brand-autodark">LOL Stats</a>
     </div>
-    <form>
+    <div class="card card-md">
       <div class="card-body">
         <h2 class="card-title text-center mb-4">Login to your account</h2>
         <div class="mb-3">
           <label class="form-label">Email address</label>
-          <input v-model="email" type="email" class="form-control" placeholder="Enter email">
+          <input v-model="form.email" type="email" class="form-control" placeholder="Enter email">
         </div>
         <div class="mb-2">
           <label class="form-label">
             Password
           </label>
           <div class="input-group input-group-flat">
-            <input v-model="password" type="password" class="form-control"  placeholder="Password"  autocomplete="off">
+            <input v-model="form.password" type="password" class="form-control"  placeholder="Password"  autocomplete="off">
             <span class="input-group-text">
               <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="2" /><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" /></svg>
@@ -34,7 +34,7 @@
           <button class="btn btn-primary w-100" @click="login()">Sign in</button>
         </div>
       </div>
-    </form>
+    </div>
     <div class="text-center text-muted mt-3">
       Don't have account yet? <a href="sign-up" tabindex="-1">Sign up</a>
     </div>
@@ -47,9 +47,11 @@ export default {
   name: 'LoginPage',
   data() {
     return {
-      email: '',
-      password: '',
-      error: null
+      form: {
+        email: '',
+        password: '',
+        error: null
+      }
     }
   },
   head: {
---
+++ frontend/pages/sign-up.vue
@@ -0,0 +1,95 @@
+<template>
+<div class="page page-center">
+  <form class="container-tight py-4"  @submit.prevent="login" >
+    <div class="text-center mb-4">
+      <a href="." class="navbar-brand navbar-brand-autodark">LOL Stats</a>
+    </div>
+    <div class="card card-md">
+      <div class="card-body">
+        <h2 class="card-title text-center mb-4">Create new account</h2>
+        <div class="mb-3">
+          <label class="form-label required">Email address</label>
+          <input v-model="form.email" type="email" class="form-control" :class="{ 'is-invalid': submitted && $v.form.email.$error }" placeholder="Enter email" required>
+          <div v-if="submitted && !$v.form.email.required" class="invalid-feedback d-block">Email is required</div>
+          <div v-if="submitted && !$v.form.email.email" class="invalid-feedback d-block">Email is invalid</div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label required">Password</label>
+          <div class="input-group input-group-flat">
+            <input v-model="form.password" type="password" class="form-control" :class="{ 'is-invalid': submitted && $v.form.password.$error }" name="password" placeholder="Password" autocomplete="off" required>
+          </div>
+          <div v-if="submitted && !$v.form.password.required" class="invalid-feedback d-block">Password is required</div>
+          <div v-if="submitted && !$v.form.password.minLength" class="invalid-feedback d-block">Password must be at least 6 characters</div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label required">Confirm Password</label>
+          <div class="input-group input-group-flat">
+            <input v-model="form.confirmPassword" type="password" class="form-control" :class="{ 'is-invalid': submitted && $v.form.confirmPassword.$error }" name="confirm-password"  placeholder="Password" autocomplete="off" required>
+          </div>
+          <div v-if="submitted && !$v.form.confirmPassword.required" class="invalid-feedback d-block">Confirm Password is required</div>
+          <div v-if="submitted && !$v.form.confirmPassword.sameAsPassword" class="invalid-feedback d-block">Passwords must match</div>
+        </div>
+        <div class="form-footer">
+          <button type="submit" class="btn btn-primary w-100" @click="login">Create new account</button>
+        </div>
+      </div>
+    </div>
+    <div class="text-center text-muted mt-3">
+      Already have account? <a href="/login" tabindex="-1">Sign in</a>
+    </div>
+  </form>
+</div>
+</template>
+
+<script>
+import { required, email, sameAs, minLength, } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'SignUpPage',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+        confirmPassword: '',
+        error: null
+      },
+      submitted: false
+    }
+  },
+  head: {
+    bodyAttrs: {
+      class: 'theme-dark border-top-wide border-primary d-flex flex-column'
+    }
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      confirmPassword: {
+        required,
+        sameAsPassword: sameAs('password')
+      },
+    },
+  },
+  methods: {
+    login() {
+      this.submitted = true;
+      
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+          return;
+      }
+
+      this.$router.push('/login');
+    }
+  }
+
+}
+</script>
