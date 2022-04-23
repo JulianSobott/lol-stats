cf48bf5799452e4af9ffa69f699eda3e338b7f8f
Tim Staudenmaier <timstaudenmaier07@gmail.com>
2022-04-23 21:29:48 +0200

    Add requesting of static data from API

--- riot-api-connector/app.py
+++ riot-api-connector/app.py
@@ -1,17 +1,42 @@
 import cassiopeia
+from db_connector import db
 from cassiopeia import Patch, Summoner
 
-cassiopeia.set_riot_api_key("")
+cassiopeia.set_riot_api_key("RGAPI-eb8ae9eb-5b1f-4caa-9b94-378c2c4e5e24")
 
 
-def add_new_summoner(id: str, region: str):
-    summoner: Summoner = cassiopeia.get_summoner(id=id, region=region)
-    # check if summoner exists in database
-    # if not
-    match_history = summoner.match_history(
-        begin_time=Patch.from_str("10.12", region=region).start)
-    # store match history in db
+class riot_api_connector:
+    def __init__(self):
+        self.db = db()
+        self.db.connect()
 
+    def add_new_summoner(self, id: str, region: str):
+        summoner: Summoner = cassiopeia.get_summoner(id=id, region=region)
+        # check if summoner exists in database
+        # if not
+        match_history = summoner.match_history(
+            begin_time=Patch.from_str('10.12', region=region).start)
+        # store match history in db
 
-def update_champions():
-    
+    def update_champions(self):
+        self.db.clear_champions()
+        champions = cassiopeia.get_champions(region='EUW')
+        for champ in champions:
+            self.db.add_champion(id=champ.id, name=champ.name, icon_path=champ.image.url)
+
+    def update_summoner_spells(self):
+        self.db.clear_summoner_spells()
+        summoner_spells = cassiopeia.get_summoner_spells(region='EUW')
+        for spell in summoner_spells:
+            self.db.add_summoner_spell(id=spell.id, name=spell.name, icon_path=spell.image.url)
+
+    def update_items(self):
+        self.db.clear_items()
+        items = cassiopeia.get_items(region='EUW')
+        for item in items:
+            self.db.add_item(id=item.id, name=item.name, icon_path=item.image.url)
+
+x = riot_api_connector()
+x.update_champions()
+x.update_items()
+x.update_summoner_spells()
--- riot-api-connector/db_connector.py
+++ riot-api-connector/db_connector.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 
-class db_connector:
+class db:
     connection = None
     cursor = None
 
@@ -25,55 +25,66 @@ class db_connector:
         self.connection.commit()
 
     def add_summoner(self, puuid: str, level: int, icon_path: str, last_update_time: int):
-        sql = """INSERT INTO summoners(puuid, level, icon_path, last_update) VALUES(%s, %s, %s, %s)"""
+        sql = """INSERT INTO summoners(puuid, level, icon_path, last_update) VALUES(%s, %s, %s, %s);"""
         self.cursor.execute(sql, (puuid, level, icon_path, last_update_time))
         self.connection.commit()
 
     def update_summoner_update_time(self, puuid: str, last_update_time: int):
-        sql = """UPDATE summoners SET last_update = %s WHERE puuid = %s"""
+        sql = """UPDATE summoners SET last_update = %s WHERE puuid = %s;"""
         self.cursor.execute(sql, (last_update_time, puuid))
         self.connection.commit()
 
     def add_summoner_spell(self, id: int, name: str, icon_path:str):
-        sql = """INSERT INTO summonerspells(id, name, icon_path) VALUES (%s, %s, %s)"""
+        sql = """INSERT INTO summonerspells(id, name, icon_path) VALUES (%s, %s, %s);"""
         self.cursor.execute(sql, (id, name, icon_path))
+        self.connection.commit()
+
+    def clear_summoner_spells(self):
+        sql = """DELETE FROM summonerspells;"""
+        self.cursor.execute(sql)
         self.connection.commit()
 
     def add_champion(self, id: int, name: str, icon_path:str):
-        sql = """INSERT INTO champions(id, name, icon_path) VALUES (%s, %s, %s)"""
+        sql = """INSERT INTO champions(id, name, icon_path) VALUES (%s, %s, %s);"""
         self.cursor.execute(sql, (id, name, icon_path))
+        self.connection.commit()
+
+    def clear_champions(self):
+        sql = """DELETE FROM champions;"""
+        self.cursor.execute(sql)
         self.connection.commit()
 
     def add_item(self, id: int, name: str, icon_path:str):
-        sql = """INSERT INTO items(id, name, icon_path) VALUES (%s, %s, %s)"""
+        sql = """INSERT INTO items(id, name, icon_path) VALUES (%s, %s, %s);"""
         self.cursor.execute(sql, (id, name, icon_path))
         self.connection.commit()
 
+    def clear_items(self):
+        sql = """DELETE FROM items;"""
+        self.cursor.execute(sql)
+        self.connection.commit()
+
     def add_challenge_class(self, name: str, class_name: str):
-        sql = """INSERT INTO challengeclasses(name, class) VALUES (%s, %s)"""
+        sql = """INSERT INTO challengeclasses(name, class) VALUES (%s, %s);"""
         self.cursor.execute(sql, (name, class_name))
         self.connection.commit()
 
     def add_stat_class(self, name: str, class_name: str):
-        sql = """INSERT INTO statclasses(name, class) VALUES (%s, %s)"""
+        sql = """INSERT INTO statclasses(name, class) VALUES (%s, %s);"""
         self.cursor.execute(sql, (name, class_name))
         self.connection.commit()
 
     def add_challenge(self, name: str, summoner_id: str, total: int, average_per_game: int, highscore: int):
-        sql = """INSERT INTO challenges(name, summoner_id, total, average_per_game, highscore) VALUES (%s, %s, %s, %s, %s)"""
+        sql = """INSERT INTO challenges(name, summoner_id, total, average_per_game, highscore) VALUES (%s, %s, %s, %s, %s);"""
         self.cursor.execute(sql, (name, summoner_id, total, average_per_game, highscore))
         self.connection.commit()
 
     def add_stat(self, name: str, summoner_id: str, total: int, average_per_game: int, highscore: int):
-        sql = """INSERT INTO stats(name, summoner_id, total, average_per_game, highscore) VALUES (%s, %s, %s, %s, %s)"""
+        sql = """INSERT INTO stats(name, summoner_id, total, average_per_game, highscore) VALUES (%s, %s, %s, %s, %s);"""
         self.cursor.execute(sql, (name, summoner_id, total, average_per_game, highscore))
         self.connection.commit()
 
     def add_game(self, match_id: str, summoner_id: str, start_time: int, duration: int, win: bool, lane: str, stats: str, challenges: str):
-        sql = """INSERT INTO challenges(match_id, summoner_id, start_time, duration, win, lane, stats, challenges) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        sql = """INSERT INTO challenges(match_id, summoner_id, start_time, duration, win, lane, stats, challenges) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
         self.cursor.execute(sql, (match_id, summoner_id, start_time, duration, win, lane, stats, challenges))
         self.connection.commit()
-
-
-x = db_connector()
-x.create_tables()
