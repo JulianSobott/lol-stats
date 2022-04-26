0fba0b34b1dc2b217569055ac89e3285754d95ad
ChristianGreiner <christian.greiner@posteo.net>
2022-04-27 00:22:27 +0200

    Add nuxt auth

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -58,8 +58,41 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
   ],
 
+  // Auth options
+  auth: {
+    strategies: {
+      local: {
+        token: {
+          property: 'token',
+          global: true,
+          // required: true,
+          // type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: {
+            url: '/api/auth/login',
+            method: 'post'
+          },
+          logout: {
+            url: '/api/auth/logout',
+            method: 'post'
+          },
+          user: {
+            url: '/api/auth/user',
+            method: 'get'
+          }
+        }
+      }
+    }
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
