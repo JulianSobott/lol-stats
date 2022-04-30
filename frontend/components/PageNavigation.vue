439b6719d0309a5040bc0578309eb03f04cfbfc4
ChristianGreiner <christian.greiner@posteo.net>
2022-04-30 19:22:32 +0200

    Fix navigation

--- frontend/components/PageNavigation.vue
+++ frontend/components/PageNavigation.vue
@@ -4,22 +4,22 @@
       <div class="navbar navbar-light">
         <div class="container-xl">
           <ul class="navbar-nav">
-            <li class="nav-item" :class="{'active': activeRoute('')}">
+            <li class="nav-item" :class="{'active': activeRoute('index') }">
               <NuxtLink to="/" class="nav-link">
                 <span class="nav-link-title">Home</span>
               </NuxtLink>
             </li>
-            <li class="nav-item" :class="{'active': activeRoute('/dashboard')}">
+            <li class="nav-item" :class="{'active': activeRoute('dashboard')}">
               <NuxtLink to="/dashboard" class="nav-link">
                 <span class="nav-link-title">Dashboard</span>
               </NuxtLink>
             </li>
-            <li class="nav-item" :class="{'active': activeRoute('/competitors')}">
+            <li class="nav-item" :class="{'active': activeRoute('competitors')}">
               <NuxtLink to="/competitors" class="nav-link">
                 <span class="nav-link-title">Competitors</span>
               </NuxtLink>
             </li>
-            <li class="nav-item" :class="{'active': activeRoute('/achievements')}">
+            <li class="nav-item" :class="{'active': activeRoute('achievements')}">
               <NuxtLink to="/achievements" class="nav-link">
                 <span class="nav-link-title">Achievements</span>
               </NuxtLink>
@@ -36,7 +36,7 @@ export default {
   name: 'PageNavigation',
   methods: {
     activeRoute(route) {
-      return this.$route.path.startsWith(route)
+      return this.$route.name === route;
     }
   }
 }
