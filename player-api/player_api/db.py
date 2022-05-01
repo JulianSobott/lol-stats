9a05b2f6297ae0a72c494588629cfd9f835192f2
JulianSobott <julian.sobott@gmail.com>
2022-05-01 13:02:29 +0200

    feat(players-api): Update DB schema for Summoners

--- player-api/player_api/db.py
+++ player-api/player_api/db.py
@@ -1,23 +1,26 @@
 import os
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, ForeignKey, Enum, CheckConstraint
+from sqlalchemy.orm import relationship
 
+from player_api.models.player import TierEnum
 
-engine = create_engine(f"postgresql://postgres:{os.environ['POSTGRES_PASSWORD']}@"
-                       f"{os.environ['POSTGRES_HOST']}/postgres")
-connection = engine.connect()
 
 Base = declarative_base()
 
 
 class Summoners(Base):
-    __tablename__ = "summoners"
+    __tablename__ = "Summoners"
 
     puuid = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+    win_rate = Column(Integer, nullable=False)
     level = Column(Integer, nullable=False)
     icon_path = Column(String, nullable=False)
-    last_update = Column(String, nullable=False)
+    last_update = Column(DateTime, nullable=False)
+    tier = Column(Enum(TierEnum))
+    rank = Column(Integer, CheckConstraint("rank >= 1 AND rank <= 5"))
+    league_points = Column(Integer, CheckConstraint("league_points >= 0 AND league_points <= 100"))
 
 
 class SummonerSpells(Base):
@@ -56,7 +59,8 @@ class Games(Base):
     __tablename__ = "Games"
 
     match_id = Column(String, primary_key=True)
-    summoner_id = Column(String, ForeignKey("summoners.puuid"), nullable=False, primary_key=True)
+    summoner_id = Column(String, ForeignKey("Summoners.puuid"), nullable=False, primary_key=True)
+    champion_id = Column(Integer, ForeignKey("Champions.id"), nullable=False)
     start_time = Column(DateTime, nullable=False)
     duration = Column(Integer, nullable=False)
     win = Column(Boolean, nullable=False)
@@ -65,6 +69,12 @@ class Games(Base):
     challenges = Column(String, nullable=False)
 
     summoner = relationship("Summoners", foreign_keys=[summoner_id])
+    champion = relationship("Champions", foreign_keys=[champion_id])
 
 
-Base.metadata.create_all(engine)
+def setup_db():
+    engine = create_engine(f"postgresql://postgres:{os.environ['POSTGRES_PASSWORD']}@"
+                           f"{os.environ['POSTGRES_HOST']}/postgres")
+    engine.connect()
+    Base.metadata.create_all(engine)
+
