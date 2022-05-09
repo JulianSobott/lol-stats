5bd5efe9c55c699cfb934522636b31f627035be1
Shadrach Arulrajah <75977@studmail.htw-aalen.de>
2022-05-09 07:29:51 +0000

    fixed connection string

--- user-backend/user_backend/app.py
+++ user-backend/user_backend/app.py
@@ -14,8 +14,8 @@ from validation import user_schema
 
 def create_app():
     flask_app = Flask(__name__)
-    # flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/userbackend"
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+    # flask_app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/userbackend"
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.app_context().push()
     db.init_app(flask_app)
