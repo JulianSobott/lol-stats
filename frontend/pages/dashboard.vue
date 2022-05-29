a47f2e3ccbe0528700b7373329ea92f9e91aaaad
ChristianGreiner <christian.greiner@posteo.net>
2022-05-29 21:18:43 +0200

    Improve Recent Games Table

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -95,105 +95,7 @@
             <PlayerStatsWinrate :playerData="playerData"/>
             <PlayerStatsRank :playerData="playerData"/>
             <PlayerStatsMostPlayed :playerData="playerData"/>
-            <div class="col-12">
-              <h3 class="mb-3">Recent Games</h3>
-              <div class="card">
-                <div class="table-responsive">
-                  <table class="table table-vcenter card-table">
-                    <thead>
-                      <tr>
-                        <th class="w-1">Victory</th>
-                        <th class="text-center">Champion</th>
-                        <th class="text-center">Teammates</th>
-                        <th class="text-center">KDA</th>
-                        <th class="text-center">Duration</th>
-                        <th class="text-center">Date</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr v-for="game in recentGames" :key="game.match_id">
-                        <td>
-                          <strong v-if="game.win" class="text-green"
-                            >Victory</strong
-                          >
-                          <strong v-else class="text-red">Defeat</strong>
-                        </td>
-                        <td class="text-center">
-                          <span
-                            class="avatar avatar-s avatar-rounded m-1"
-                            :style="championIconPath(game.self.champion)"
-                          ></span>
-                          <span class="text-muted d-block">{{
-                            game.self.champion.name
-                          }}</span>
-                        </td>
-                        <td>
-                          <div class="row">
-                            <div class="col-12 col-sm-6">
-                              <TeamList :match="game" :team="game.ally_team" />
-                            </div>
-                            <div class="col-12 col-sm-6">
-                              <TeamList :match="game" :team="game.enemy_team" />
-                            </div>
-                          </div>
-                        </td>
-                        <td class="text-center">
-                          {{ game.self.stats.kills }} /
-                          {{ game.self.stats.deaths }} /
-                          {{ game.self.stats.assists }}
-                        </td>
-                        <td class="text-center">
-                          {{ converDuration(game.duration) }}
-                        </td>
-                        <td class="text-center">
-                          {{ converTimestamp(game.timestamp) }}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div class="card-footer">
-                  <div class="text-center">
-                    <button
-                      class="btn btn-primary ms-auto"
-                      @click="loadMoreGames()"
-                      :disabled="!getNextRecentGamesLink()"
-                    >
-                      <span
-                        v-if="moreGamesLoading"
-                        class="spinner-border spinner-border-sm icon icon-tabler"
-                        role="status"
-                        aria-hidden="true"
-                      ></span>
-                      <svg
-                        v-else
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="icon icon-tabler icon-tabler-refresh"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <path
-                          stroke="none"
-                          d="M0 0h24v24H0z"
-                          fill="none"
-                        ></path>
-                        <path
-                          d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4"
-                        ></path>
-                        <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"></path>
-                      </svg>
-                      Load More
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PlayerStatsRecentGames ref="recentGames" :playerUuid="$auth.user.player_stats.id" />
           </div>
         </div>
       </div>
@@ -202,8 +104,7 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import moment from 'moment'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'IndexPage',
@@ -212,16 +113,12 @@ export default {
     playerData() {
       return this.$store.state.dashboard.playerData
     },
-    recentGames() {
-      return this.$store.state.dashboard.recentGames
-    },
   },
   mounted() {
     this.fetchUserData()
-    this.clearRecentGames()
     if (this.$auth.user.player_stats.imported) {
       this.getPlayerData()
-      this.getRecentGames()
+      this.$refs.recentGames.fetchPlayerData()
     } else {
       this.isImportingData = true
       this.importPlayerData()
@@ -236,7 +133,6 @@ export default {
   data() {
     return {
       isImportPlayerData: false,
-      moreGamesLoading: false,
       importInterval: null,
       importData: {
         import_state: 'PENDING',
@@ -248,13 +144,8 @@ export default {
     }
   },
   methods: {
-    ...mapGetters({
-      getNextRecentGamesLink: 'dashboard/getNextRecentGamesLink',
-    }),
     ...mapActions({
       getPlayerData: 'dashboard/getPlayerData',
-      getRecentGames: 'dashboard/getRecentGames',
-      loadMoreRecentGames: 'dashboard/loadMoreRecentGames',
       clearRecentGames: 'dashboard/clearRecentGames',
     }),
     championIconPath(champion) {
@@ -281,21 +172,6 @@ export default {
     async fetchUserData() {
       await this.$auth.fetchUser()
       this.isImportPlayerData = !this.$auth.user.player_stats.imported
-    },
-    converTimestamp(value) {
-      return moment(String(value)).format('MM-DD-YYYY')
-    },
-    converDuration(secs) {
-      return moment.utc(secs * 1000).format('mm:ss')
-    },
-    async loadMoreGames() {
-      try {
-        this.moreGamesLoading = true
-        await this.loadMoreRecentGames()
-        this.moreGamesLoading = false
-      } catch (e) {
-        console.log(e)
-      }
     },
   },
 }
--- frontend/store/dashboard.js
+++ frontend/store/dashboard.js
@@ -1,8 +1,6 @@
 // state
 export const state = () => ({
   playerData: {},
-  recentGames: [],
-  nextRecentGamesLink: ''
 })
 
 // getters
@@ -10,12 +8,6 @@ export const getters = {
   getPlayerData(state) {
     return state.playerData;
   },
-  getRecentGames(state) {
-    return state.recentGames;
-  },
-  getNextRecentGamesLink(state) {
-    return state.nextRecentGamesLink;
-  }
 }
 
 // actions
@@ -28,29 +20,6 @@ export const actions = {
     const response = await this.$axios.get(`/players/${playerUuid}`);
     commit("setPlayerData", response.data);
   },
-  async getRecentGames({
-    commit
-  }) {
-    const playerUuid = this.$auth.user.player_uuid
-    const response = await this.$axios.get(`/players/${playerUuid}/recent-games`);
-    commit("setRecentGames", response.data.items);
-    commit("setNextRecentGamesLink", response.data.next);
-  },
-  async loadMoreRecentGames({
-    commit,
-    state
-  }) {
-    if (state.nextRecentGamesLink !== null) {
-      const response = await this.$axios.get(state.nextRecentGamesLink);
-      commit("setRecentGames", response.data.items);
-      commit("setNextRecentGamesLink", response.data.next);
-    }
-  },
-  clearRecentGames({
-    commit,
-  }) {
-    commit("clearRecentGames");
-  },
   clearStore({ commit }) {
     commit("clearStore")
   }
@@ -61,18 +30,7 @@ export const mutations = {
   setPlayerData(state, data) {
     state.playerData = data;
   },
-  setRecentGames(state, data) {
-    state.recentGames.push(...data);
-  },
-  setNextRecentGamesLink(state, data) {
-    state.nextRecentGamesLink = data;
-  },
-  clearRecentGames(state) {
-    state.recentGames = [];
-  },
   clearStore(state) {
-    state.recentGames = [];
     state.playerData = {};
-    state.nextRecentGamesLink = '';
   },
 }
