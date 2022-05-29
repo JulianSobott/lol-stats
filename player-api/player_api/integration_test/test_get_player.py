6622df35619477054642754a0908e5f1a7aecc5e
JulianSobott <julian.sobott@gmail.com>
2022-05-29 13:31:46 +0200

    fix(player_api/get_player): Use riot API when player is incomplete in DB

---
+++ player-api/player_api/integration_test/test_get_player.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+
+from player_api.db import Summoners
+from player_api.main import app
+from player_api.models.player import Player, PlayerId
+
+PLAYER_ID = "TEST_PUUID"
+
+client = TestClient(app)
+
+
+def test_get_incomplete_db_player(db_session, requests_mock):
+    requests_mock.real_http = True
+    requests_mock.get(
+        f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{PLAYER_ID}",
+        json={"name": "xyz", "puuid": PLAYER_ID, "summonerLevel": 1},
+    )
+
+    player = Summoners(puuid=PLAYER_ID, region_id=PLAYER_ID, name="xyz", level=1)
+    db_session.add(player)
+    db_session.commit()
+    response_player = _player_reqeust(PLAYER_ID)
+    assert response_player.id == PLAYER_ID
+    assert response_player.imported is False
+
+
+def _player_reqeust(player_id: PlayerId) -> Player:
+    response = client.get(f"/players/{player_id}")
+    assert response.status_code == 200
+    return Player(**response.json())
