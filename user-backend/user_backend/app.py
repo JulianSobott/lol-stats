5ad246a3c695d201bda9f0ded1da525cf272bba0
Shadrach Arulrajah <shadrach@live.de>
2022-06-05 11:23:00 +0200

    added achievement model & database

--- user-backend/user_backend/app.py
+++ user-backend/user_backend/app.py
@@ -44,6 +44,7 @@ class Users(db.Model):
     region = db.Column(db.String(20), default="euw")
     competitors = db.relationship('Competitors', backref='user', lazy=True)
     access_token = db.relationship('AccessToken', backref='user', lazy=True)
+    favourite_achievement = db.relationship('FavouriteAchievement', backref='user', lazy=True)
 
 
 class Competitors(db.Model):
@@ -61,6 +62,13 @@ class AccessToken(db.Model):
     token = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.TIMESTAMP, nullable=False)
     updated_at = db.Column(db.TIMESTAMP, nullable=False)
+
+
+class FavouriteAchievement(db.Model):
+    __tablename__ = 'favourite_achievement'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
 
 
 def get_token(header):
